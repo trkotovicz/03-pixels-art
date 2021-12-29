120fe0ced92b8b0978ad3b73bead85aeb876a6ef
Thais Kotovicz <thais_kotovicz@hotmail.com>
2021-12-29 00:22:38 -0300

    Requisitos 6 e 7 ok

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-window.onload = corInicial;
+// Requisito 6
+window.onload = inicialBlack;
 
-function corInicial() {
-    document.getElementsByClassName("selected")
-}
+function inicialBlack() {
+    let black = document.getElementById("black");
+    black.classList.add("selected");
+};
+
+// Requisito 7
+let cores = document.querySelectorAll(".color");
+
+//o for percorre as cores e add o escutador de click em cada uma delas. Também chama a função "selectedColor" no escutador.
+for (let index = 0; index < cores.length; index += 1) {
+    cores[index].addEventListener('click', selectedColor);
+};
+
+//remove a classe "selected" já existente e adiciona ela no novo elemento (o que foi clicado).
+function selectedColor(event) {
+    for (let index = 0; index < cores.length; index += 1 ) {
+        cores[index].classList.remove("selected");
+    };
+    event.target.classList.add("selected");
+};
+
+// Requisito 8
